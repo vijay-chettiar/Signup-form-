68f83aabf73847a695a445e790d80aae369a5e2e
yvjchettiar <chettiar.vj23@gmail.com>
2022-09-11 17:52:36 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Signup from "./Signup";
+
+const App = () => {
+  return (
+    <div id="main">
+    <Signup/>
+    
+      
+  
+        
+      
+    </div>
+  )
+}
+
+
+export default App;
